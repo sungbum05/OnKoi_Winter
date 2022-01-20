e9aceaa3fcd4fafd95792d049719fed8e1074e02
wtsiwon <siwon4727@gmail.com>
2022-01-21 02:05:51 +0900

    응애..?

---
+++ Assets/Siwon/Script/Item.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Item : MonoBehaviour
+{
+
+    protected virtual void OnTriggerEnter(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+        }
+    }
+
+
+}
