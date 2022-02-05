1af407476ba358900c4ffade582ab93c852c55e2
wtsiwon <siwon4727@gmail.com>
2022-02-05 23:11:44 +0900

    핵 만드는중

--- Assets/Siwon/Script/ItemSkill.cs
+++ Assets/Siwon/Script/ItemSkill.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemSkill : Item
+public class ItemSkill : MonoBehaviour
 {
     public enum ItemSkillType
     {
--- Assets/Siwon/Script/ItemSpawner.cs
+++ Assets/Siwon/Script/ItemSpawner.cs
@@ -2,26 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemSpawner : Item
+public class ItemSpawner : MonoBehaviour
 {
     [SerializeField]
     public List<GameObject> ItemMenu = new List<GameObject>();
 
     public float Radius = 5;
-
+    public Vector3 ItemPosition;
     void Start()
     {
         SpawnItem();
     }
     IEnumerator Wait()
     {
-        yield return new WaitForSeconds(10f);
+        yield return new WaitForSeconds(2f);
         SpawnItem();
     }
     void SpawnItem()
     {
         this.transform.position = new Vector3(Random.insideUnitSphere.x, 0, Random.insideUnitSphere.z);
-        Instantiate(ItemMenu[Random.Range(0, ItemMenu.Count)], this.transform.position * Radius, Quaternion.identity);
+        ItemPosition = this.transform.position;
+        Instantiate(ItemMenu[Random.Range(0, ItemMenu.Count)], ItemPosition * Radius, Quaternion.identity);
         StartCoroutine(Wait());
         
     }
