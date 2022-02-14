bc2064ad2a117402709312fba3d8446391025c89
wtsiwon <siwon4727@gmail.com>
2022-02-14 10:35:45 +0900

    저장

--- Assets/Siwon/Script/Item/mBoming.cs
+++ Assets/Siwon/Script/Item/mBoming.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 
 public class mBoming : MonoBehaviour
 {
-    void OnTriggerEnter(Collider2D collision)
+    Unit unit;
+    Collider thisCollider;
+    void OnTriggerEnter(Collider collision)
     {
-       
+        if (collision.tag == "Enemy")
+        {
+            unit.Hp -= 1000;
+        }
     }
+    private void Start()
+    {
+        
+        thisCollider = GetComponent<Collider>();
+        gameObject.SetActive(true);
+        OnTriggerEnter(thisCollider);
+        gameObject.SetActive(false);
+    }
+   
 }
